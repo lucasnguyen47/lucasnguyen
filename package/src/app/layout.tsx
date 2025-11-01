d82ea57ea1c03d9c6c1699056bd42b63d59d4bfe
Nguyễn Thành Hòa <lucasnguyen@Mac-mini-cua-Nguyen.local>
2025-11-01 14:50:00 +0700

    speed insight

--- package/src/app/layout.tsx
+++ package/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const bricolageGrotesque = Bricolage_Grotesque({
   variable: "--font-geist-sans",
@@ -20,7 +21,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en" suppressHydrationWarning>
+      <SpeedInsights />
       <body className={bricolageGrotesque.className}>
         <Header />
         {children}
